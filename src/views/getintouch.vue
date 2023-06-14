beb20dc28995820123d152aadb10ac2fd3adebbe
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2023-06-14 13:50:12 +0100

    the more

---
+++ src/views/getintouch.vue
@@ -0,0 +1,416 @@
+<script setup>
+import footerpage from '@/components/footerpage.vue'
+import whiteNav from '@/components/whiteNav.vue'
+
+</script>
+
+<template>
+    <whiteNav/>
+   <div class="getting-in-touch">
+        <div class="getting-in-touch-first">
+            <h3>Get in touch</h3>
+            <p>We are here to support you throughout the process of getting your business listed.</p>
+        </div>
+        <div class="the-customers-support">
+            <div class="customers-support">
+                <div class="each-support">
+                    <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M4 17C4 10 8 7 14 7H34C40 7 44 10 44 17V31C44 38 40 41 34 41H14" stroke="#1E1E4B" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M34 18L27.74 23C25.68 24.64 22.3 24.64 20.24 23L14 18" stroke="#1E1E4B" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M4 33H16" stroke="#1E1E4B" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M4 25H10" stroke="#1E1E4B" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                    <p class="each-support-p">Customer support</p>
+                </div>
+                <p class="customer-support-p">Please fill out the form below</p>
+            </div>
+            <div class="customers-support">
+                <div class="each-support">
+                    <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M4.58008 15.56V35.02C4.58008 38.82 7.28008 40.38 10.5601 38.5L15.2601 35.82C16.2801 35.24 17.9801 35.18 19.0401 35.72L29.5401 40.98C30.6001 41.5 32.3001 41.46 33.3201 40.88L41.9801 35.92C43.0801 35.28 44.0001 33.72 44.0001 32.44V12.98C44.0001 9.18 41.3001 7.62 38.0201 9.5L33.3201 12.18C32.3001 12.76 30.6001 12.82 29.5401 12.28L19.0401 7.04C17.9801 6.52 16.2801 6.56 15.2601 7.14L6.60008 12.1C5.48008 12.74 4.58008 14.3 4.58008 15.56Z" stroke="#1E1E4B" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M17.1191 8V34" stroke="#1E1E4B" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M31.4609 13.24V40" stroke="#1E1E4B" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+                    <p class="each-support-p">Business address</p>
+                </div>
+                <p class="customer-support-p">Suites (C113 & C114) Akord Shopping Mall,
+Bogije, Elemoro, 101001, 
+Lagos, Nigeria</p>
+            </div>
+            <div class="customers-support">
+                <div class="each-support">
+                    <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M41.94 34.66C41.94 35.38 41.78 36.12 41.44 36.84C41.1 37.56 40.66 38.24 40.08 38.88C39.1 39.96 38.02 40.74 36.8 41.24C35.6 41.74 34.3 42 32.9 42C30.86 42 28.68 41.52 26.38 40.54C24.08 39.56 21.78 38.24 19.5 36.58C17.2 34.9 15.02 33.04 12.94 30.98C10.88 28.9 9.02 26.72 7.36 24.44C5.72 22.16 4.4 19.88 3.44 17.62C2.48 15.34 2 13.16 2 11.08C2 9.72 2.24 8.42 2.72 7.22C3.2 6 3.96 4.88 5.02 3.88C6.3 2.62 7.7 2 9.18 2C9.74 2 10.3 2.12 10.8 2.36C11.32 2.6 11.78 2.96 12.14 3.48L16.78 10.02C17.14 10.52 17.4 10.98 17.58 11.42C17.76 11.84 17.86 12.26 17.86 12.64C17.86 13.12 17.72 13.6 17.44 14.06C17.18 14.52 16.8 15 16.32 15.48L14.8 17.06C14.58 17.28 14.48 17.54 14.48 17.86C14.48 18.02 14.5 18.16 14.54 18.32C14.6 18.48 14.66 18.6 14.7 18.72C15.06 19.38 15.68 20.24 16.56 21.28C17.46 22.32 18.42 23.38 19.46 24.44C20.54 25.5 21.58 26.48 22.64 27.38C23.68 28.26 24.54 28.86 25.22 29.22C25.32 29.26 25.44 29.32 25.58 29.38C25.74 29.44 25.9 29.46 26.08 29.46C26.42 29.46 26.68 29.34 26.9 29.12L28.42 27.62C28.92 27.12 29.4 26.74 29.86 26.5C30.32 26.22 30.78 26.08 31.28 26.08C31.66 26.08 32.06 26.16 32.5 26.34C32.94 26.52 33.4 26.78 33.9 27.12L40.52 31.82C41.04 32.18 41.4 32.6 41.62 33.1C41.82 33.6 41.94 34.1 41.94 34.66Z" stroke="#1E1E4B" stroke-width="3" stroke-miterlimit="10"/>
+<path d="M35 16C35 14.8 34.06 12.96 32.66 11.46C31.38 10.08 29.68 9 28 9" stroke="#1E1E4B" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M42 16C42 8.26 35.74 2 28 2" stroke="#1E1E4B" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+                    <p class="each-support-p">Phone number</p>
+                </div>
+                <p class="customer-support-p">+234 903 921 6724</p>
+            </div>
+        </div>
+        <div class="getstarted-div-form moving-low">
+            <form action="" class="getstarted-form">
+                <label for="">FULL name</label>
+                <input type="text">
+                <label for="">Email</label>
+                <input type="email">
+                <label for="">Business type (Optional)</label>
+                <input type="text">
+                <label for="" >Password</label>
+                <div class="passwordpart">
+                    <input type="text" >
+                    <svg class="eyes" width="18" height="10" viewBox="0 0 18 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1.24106 0.745899C1.53326 0.44784 1.99051 0.420743 2.31272 0.66461L2.40503 0.745899L9 7.47342L15.595 0.745899C15.8872 0.44784 16.3444 0.420743 16.6666 0.66461L16.7589 0.745899C17.0511 1.04396 17.0777 1.51037 16.8386 1.83904L16.7589 1.93321L9.58198 9.2541C9.28978 9.55216 8.83254 9.57926 8.51033 9.33539L8.41802 9.2541L1.24106 1.93321C0.919645 1.60534 0.919645 1.07376 1.24106 0.745899Z" fill="#603F8B" stroke="#603F8B"/>
+                    </svg>
+
+                </div>
+                <div class="getstarted-check1">
+                    <!-- <input type="textarea" class="the-check1"> -->
+                    <textarea class="textarea-one" name="" id="" cols="30" rows="10"></textarea>
+                </div>
+                <button class="getstarted-btn-new">Send message</button>
+
+            </form>
+        </div>
+   </div>
+    <footerpage/>
+</template>
+<style>
+.getting-in-touch-first{
+    /* border:2px solid yellow; */
+    background-color: #532F82;
+    background-image: url("@/assets/backgroungontouch.png");
+    background-size:contain;
+    height:250px;
+    padding-top:2%;
+    background-repeat: repeat;
+
+}
+.getting-in-touch-first h3{
+    font-family: 'Recoleta Alt';
+font-style: normal;
+font-weight: 600;
+font-size: 36px;
+line-height: 49px;
+/* identical to box height */
+text-align: center;
+color: #FFFFFF;
+}
+.getting-in-touch-first p{
+    font-family: 'Circular Std';
+    font-style: normal;
+    font-weight: 450;
+    font-size: 20px;
+    line-height: 143.02%;
+    /* identical to box height, or 29px */
+    text-align: center;
+    color: #EDDFFF;
+
+}
+
+/* the  first section */
+.the-customers-support{
+    /* border:2px solid yellow; */
+    margin-top:-5%;
+  
+    display:flex;
+    justify-content: space-around;
+    align-content: center;
+    
+}
+.customers-support{
+    /* border:2px solid blue; */
+    /* padding:2% 4%; */
+    width:25%;
+    box-sizing: border-box;
+height: 167px;
+background: #FFFFFF;
+border: 2px solid #EEEEFF;
+border-radius: 12px;
+
+
+}
+.each-support{
+    /* border:2px solid green; */
+    width:80%;
+    margin:auto;
+    margin-top:5%;
+    display:flex;
+    justify-content: space-around;
+    align-items: center;
+    /* gap:2%; */
+   
+}
+.each-support svg{
+    /* border:2px solid red; */
+    width:50px;
+}
+.each-support-p{
+    /* border:2px solid yellow; */
+font-family: 'Circular Std';
+font-style: normal;
+font-weight: 500;
+font-size: 20px;
+line-height: 30px;
+
+color: #1E1E4B;
+}
+.customer-support-p{
+    /* border:2px solid yellow; */
+    width:80%;
+    margin:auto;
+    margin-top: 8%;
+    
+font-family: 'Circular Std';
+font-style: normal;
+font-weight: 450;
+font-size: 18px;
+line-height: 23px;
+text-align: center;
+
+color: #656565;
+
+}
+/* form part */
+/* .getstarted-div-form{
+    border:2px solid yellow;
+} */
+.getstarted-form{
+    /* border:2px solid yel/low; */
+    display:grid;
+    gap:1rem;
+    width:40%;
+    margin:auto;
+}
+.getstarted-form label{
+    font-family: 'Circular Std';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 23px;
+    /* identical to box height */
+    letter-spacing: -0.005em;
+    color: #603F8B;
+    
+}
+.getstarted-form input{
+    box-sizing: border-box;
+    width: 486px;
+    height: 50px;
+    outline: none;
+    font-size:20px;
+    background: #F8FAFD;
+    border: 2px solid #E7EAF1;
+    border-radius: 6px;
+}
+
+.textarea-one{
+    border:2px solid green; box-sizing: border-box;
+    width: 486px;
+    height: 120px;
+    outline: none;
+    font-size:20px;
+    /* margin-left:10%; */
+    background: #F8FAFD;
+    border: 2px solid #E7EAF1;
+    border-radius: 6px;
+    /* margin-left:-50%; */
+}
+
+
+/* border:2px solid green; */
+.getstarted-btn-new{
+    background: #532F82;
+box-shadow: 2px 8px 8px rgba(86, 86, 86, 0.25);
+border-radius: 6px;
+font-family: 'Circular Std';
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 143.02%;
+/* identical to box height, or 26px */
+border:none;
+margin-top:5%;
+color: #FFFFFF;
+height: 58px;
+margin-bottom: 15%;
+/* left: 477px; */
+/* top: 1065px; */
+}
+.moving-low{
+    margin-top:10%;
+}
+@media screen and (max-width: 900px){
+    .getting-in-touch-first{
+    /* border:2px solid yellow; */
+    background-color: #532F82;
+    background-image: url("@/assets/backgroungontouch.png");
+    background-size:contain;
+    height:250px;
+    padding-top:2%;
+    background-repeat: repeat;
+
+}
+.getting-in-touch-first h3{
+    font-family: 'Recoleta Alt';
+font-style: normal;
+font-weight: 600;
+font-size: 26px;
+line-height: 49px;
+/* identical to box height */
+text-align: center;
+color: #FFFFFF;
+}
+.getting-in-touch-first p{
+    font-family: 'Circular Std';
+    font-style: normal;
+    font-weight: 450;
+    font-size: 15px;
+    width:80%;
+    margin:auto;
+    line-height: 143.02%;
+    /* identical to box height, or 29px */
+    text-align: center;
+    color: #EDDFFF;
+
+}
+
+/* the  first section */
+.the-customers-support{
+    /* border:2px solid yellow; */
+    margin-top:-8%;
+    margin-bottom:20%;
+    gap:8%;
+    display:grid;
+    grid-template-columns: 1fr;
+    /* justify-content: center; */
+    /* align-content: center; */
+    
+}
+.customers-support{
+    /* border:2px solid blue; */
+    /* padding:2% 4%; */
+    width:70%;
+    margin:auto;
+    box-sizing: border-box;
+height: 167px;
+background: #FFFFFF;
+border: 2px solid #EEEEFF;
+border-radius: 12px;
+
+
+}
+.each-support{
+    /* border:2px solid green; */
+    width:80%;
+    margin:auto;
+    margin-top:5%;
+    display:flex;
+    justify-content: space-around;
+    align-items: center;
+    /* gap:2%; */
+   
+}
+.each-support svg{
+    /* border:2px solid red; */
+    width:50px;
+}
+.each-support-p{
+    /* border:2px solid yellow; */
+font-family: 'Circular Std';
+font-style: normal;
+font-weight: 500;
+font-size: 20px;
+line-height: 30px;
+
+color: #1E1E4B;
+}
+.customer-support-p{
+    /* border:2px solid yellow; */
+    width:80%;
+    margin:auto;
+    margin-top: 8%;
+    
+font-family: 'Circular Std';
+font-style: normal;
+font-weight: 450;
+font-size: 18px;
+line-height: 23px;
+text-align: center;
+
+color: #656565;
+
+}
+/* form part */
+/* .getstarted-div-form{
+    border:2px solid yellow;
+} */
+.getstarted-form{
+    /* border:2px solid yel/low; */
+    display:grid;
+    gap:1rem;
+    width:40%;
+    margin:auto;
+}
+.getstarted-form label{
+    font-family: 'Circular Std';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 23px;
+    /* identical to box height */
+    letter-spacing: -0.005em;
+    color: #603F8B;
+    
+}
+.getstarted-form input{
+    box-sizing: border-box;
+    width: 486px;
+    height: 50px;
+    outline: none;
+    font-size:20px;
+    background: #F8FAFD;
+    border: 2px solid #E7EAF1;
+    border-radius: 6px;
+}
+
+.textarea-one{
+    border:2px solid green; box-sizing: border-box;
+    width: 286px;
+    height: 120px;
+    outline: none;
+    font-size:20px;
+    /* margin-left:10%; */
+    background: #F8FAFD;
+    border: 2px solid #E7EAF1;
+    border-radius: 6px;
+    /* margin-left:-50%; */
+}
+
+
+/* border:2px solid green; */
+.getstarted-btn-new{
+    background: #532F82;
+box-shadow: 2px 8px 8px rgba(86, 86, 86, 0.25);
+border-radius: 6px;
+font-family: 'Circular Std';
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 143.02%;
+/* identical to box height, or 26px */
+border:none;
+margin-top:5%;
+color: #FFFFFF;
+height: 58px;
+margin-bottom: 15%;
+/* left: 477px; */
+/* top: 1065px; */
+}
+.moving-low{
+    margin-top:10%;
+}
+}
+</style>
